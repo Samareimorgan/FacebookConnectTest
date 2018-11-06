96ddfad855f405934522096e5fdb7ec38dbdd28a
Samantha <samareimorgan@gmail.com>
2018-11-05 19:52:07 -0700

    Add facebook.js link

--- public/assets/javascript/facebook.js
+++ public/assets/javascript/facebook.js
@@ -1,79 +1,41 @@
-// //Account Kit Javascript SDK for Authentication
+
+window.fbAsyncInit = function() {
+
+      FB.init({
+        appId      : '290716314893480',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v3.2'
+      });
+        
+      
+      FB.getLoginStatus(function(response) {
+        console.log(response);
+        console.log(response.authResponse);
+        console.log(response.authResponse.accessToken);
+        console.log(response.authResponse.userID);
+          statusChangeCallback(response);
+        testAPI();
+      });
+        
+    };
+  
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
 
 
-//         //<!--HTTPS required. HTTP will give a 403 forbidden response -->
-//         <script src="https://sdk.accountkit.com/en_US/sdk.js"></script>
-
-//         //Add UI Elements for Account Kit to HTML
-//             //<input placeholder="email" id="email"/>
-//             //<button onclick="emailLogin();">Login via Email</button>
-    
-
-
-//   // initialize Account Kit with CSRF protection
-//   AccountKit_OnInteractive = function(){
-//     AccountKit.init(
-//       {
-//         appId:"{{FACEBOOK_APP_ID}}", 
-//         state:"{{csrf}}", 
-//         version:"{{ACCOUNT_KIT_API_VERSION}}",
-//         fbAppEventsEnabled:true,
-//         redirect:"{{REDIRECT_URL}}"
-//       }
-//     );
-//   };
-
-//   // login callback
-//   function loginCallback(response) {
-//     if (response.status === "PARTIALLY_AUTHENTICATED") {
-//       var code = response.code;
-//       var csrf = response.state;
-//       // Send code to server to exchange for access token
-//     }
-//     else if (response.status === "NOT_AUTHENTICATED") {
-//       // handle authentication failure
-//     }
-//     else if (response.status === "BAD_PARAMS") {
-//       // handle bad parameters
-//     }
-//   }
-
-//   // phone form submission handler
-//   function smsLogin() {
-//     var countryCode = document.getElementById("country_code").value;
-//     var phoneNumber = document.getElementById("phone_number").value;
-//     AccountKit.login(
-//       'PHONE', 
-//       {countryCode: countryCode, phoneNumber: phoneNumber}, // will use default values if not specified
-//       loginCallback
-//     );
-//   }
-
-
-//   // email form submission handler
-//   function emailLogin() {
-//     var emailAddress = document.getElementById("email").value;
-//     AccountKit.login(
-//       'EMAIL',
-//       {emailAddress: emailAddress},
-//       loginCallback
-//     );
-//   }
-
-
-    
-
-
-//FaceBook Javascript SDK for Authentication //
-
-
-    
 
 //Functions for Facebook 
 
   function statusChangeCallback(response) {
     if(response.status === "connected") {
       console.log( "logged in and authenticated");
+      //if connected - then take the user id, email and name and push to the UserTable in Sequelize
       //setElements(true);
      // testAPI();
     } else {
@@ -88,19 +50,19 @@
       statusChangeCallback(response);
     });
   }
-  function setElements(isLoggedIn) {
-    if(isLoggedIn) {
-      document.getElementById('logout').style.display = "block";
-      document.getElementById('profile').style.display ="block";
-      document.getElementById ('fb-btn').style.display ="none";
-    } else
-      document.getElementById('logout').style.display = "none";
-      document.getElementById('profile').style.display ="none";
-      document.getElementById ('fb-btn').style.display ="block";
-  }
+  // function setElements(isLoggedIn) {
+  //   if(isLoggedIn) {
+  //     document.getElementById('logout').style.display = "block";
+  //     document.getElementById('profile').style.display ="block";
+  //     document.getElementById ('fb-btn').style.display ="none";
+  //   } else
+  //     document.getElementById('logout').style.display = "none";
+  //     document.getElementById('profile').style.display ="none";
+  //     document.getElementById ('fb-btn').style.display ="block";
+  // }
   function logout() {
     FB.logout(function(response){
-      setElements(false);
+      //setElements(false);
     })
   }
 
