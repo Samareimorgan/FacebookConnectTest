752d98143ee5053c632798731f6d6d27b8947a39
Samantha <samareimorgan@gmail.com>
2018-11-06 16:53:41 -0700

    Add build profile function

--- public/assets/javascript/facebook.js
+++ public/assets/javascript/facebook.js
@@ -80,13 +80,14 @@
     })
   }
 
-  function testAPI() {
+  function facebookAPI() {
     FB.api('/me?fields=id,name,email', function(response){
       if(response && !response.error){
         console.log(response);
         var userName = response.name;
         var userId = response.id;
-       // var userEmail = response.email;
+        var userPic = response.picture;
+       var userEmail = response.email;
         console.log(name +  userId);
         enterUser(userId, userName);
         //buildProfile(user);
@@ -100,17 +101,15 @@
     console.log("pushed user data");
 }
 
+function buildProfile(userName, userEmail, userPic) {
+
+  $("#profileName").text(userName);
+  $("#profileEmail").text(userEmail);
+  $("#profilePhoto").attr("src", userPic);
+};
 
   
-  function buildProfile(user) {
-    let profile = `
-      <h2>${user.name} </h2>
-      <h3>${user.email}</h3>
-    `;
-
-      document.getElementById("profile").innerHTML = profile;
-  }
-           
+    
 // //Facebook Login Button HTML Code
 //   <fb:login-button 
 //   id = "fb-btn"
